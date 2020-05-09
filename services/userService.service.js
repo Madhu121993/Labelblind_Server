addb7e12a22866d1210698dfc9dcbaaf8de924a1
unknown <madhujha2013@gmail.com>
2020-05-09 22:34:12 +0530

    sendmail function added and json related operations performed

--- services/userService.service.js
+++ services/userService.service.js
@@ -12,6 +12,7 @@ const rp = require('request-promise');
 var path = require('path');
 var config = require('../config/config')
 var currencyConversion = require('./currencyConversion.service')
+const sgMail = require('@sendgrid/mail');
 
 service.getUserDetails = getUserDetails;
 module.exports = service;
@@ -22,12 +23,50 @@ var options = {
     quote     : '"' // optional
 };
 
+function sendMail(req,res){
+    var deffered = Q.defer();
+    sgMail.setApiKey('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');
+const msg = {
+  to: 'krnarvekar@gmail.com',
+  from: 'kuldeepnarvekar2017@gmail.com',
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+};
+// sgMail.send(msg);
+// // deffered.resolve({data:"data"})
+// // return deffered.promise;
+// }
+
+sgMail
+  .send(msg)
+  .then(() => {}, error => {
+    console.error(error);
+
+    if (error.response) {
+      console.error(error.response.body)
+    }
+  });
+}
+
 // Get User Details from DB
 function getUserDetails(req,res){
-    var file_data = fs.readFileSync(path.join(__dirname, '../Files/Files1.csv'), { encoding : 'utf8'});
+    var fileName = 'Files1.csv'
+    var file_data = fs.readFileSync(path.join(__dirname, '../Files/'+fileName), { encoding : 'utf8'});
     var result = csvjson.toObject(file_data, options);
     console.log("requestedData is",result)
     var deffered = Q.defer();
+    // var date = new Date();
+    // var usdValues = result.map(function(item){
+    //     var info = { "Amount:": Number(item.Amount),
+    //     "Currency:": item.Currency,
+    //     "convertedAmount": item.Amount,
+    //     "convertedCurrency": 'USD',
+    //     "Date": date
+    //    }
+    //    return info;
+    //        });
+
     currencyConversion.getCurrencies(req,res).then(function(data) {
         console.log("currencyConversion response:",data["usdValue"])
         var date = new Date();
@@ -41,7 +80,19 @@ function getUserDetails(req,res){
         return info;
         });
         console.log(usdValues);
-        deffered.resolve(usdValues);
+        var max =  Math.max.apply(Math, usdValues.map(function(o) { 
+            return o.convertedAmount;  
+        }))
+        var min =  Math.min.apply(Math, usdValues.map(function(o) { 
+            return o.convertedAmount;  
+        }))
+        var sum = usdValues.reduce(function (res, item) {
+            return res + Number(item.convertedAmount);
+          }, 0);
+
+        var average = sum / usdValues.length;
+        console.log("Max: ",max,min,sum,average)
+        deffered.resolve({usdValues:usdValues,max:max,min:min,sum:sum,average:average});
     })
     .catch(function(err) {
         res.status(400).send(err);
